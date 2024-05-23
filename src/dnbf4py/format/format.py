1d87e6d9705cfc52e1332fd52836fde1e11a01ea
yntha <126660548+yntha@users.noreply.github.com>
2024-05-23 11:43:56 -0400

    Run ruff format on format.py

--- src/dnbf4py/format/format.py
+++ src/dnbf4py/format/format.py
@@ -6,11 +6,11 @@ from datastream import DeserializingStream, SerializingStream
 class DNBinaryFormat:
     def __init__(self, stream: DeserializingStream):
         self.stream = stream
-    
+
     @classmethod
     def from_bytes(cls, data: bytes):
         return cls(DeserializingStream(data))
-    
+
     @classmethod
     def from_file(cls, path: str | Path):
         path = Path(path)
