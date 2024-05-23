9b90d3ef503c1a5b8395d0e76f7d0ea18da098af
yntha <126660548+yntha@users.noreply.github.com>
2024-05-23 13:37:29 -0400

    Move `RecordTypeEnum` to `types.py`

--- src/dnbf4py/format/format.py
+++ src/dnbf4py/format/format.py
@@ -5,30 +5,7 @@ from typing import Self
 
 from datastream import DeserializingStream, SerializingStream
 
-from dnbf4py.format.types import Record, RecordTypes
-
-
-class RecordTypeEnum(IntEnum):
-    SerializedStreamHeader = 0
-    ClassWithId = 1
-    SystemClassWithMembers = 2
-    ClassWithMembers = 3
-    SystemClassWithMembersAndTypes = 4
-    ClassWithMembersAndTypes = 5
-    BinaryObjectString = 6
-    BinaryArray = 7
-    MemberPrimitiveTyped = 8
-    MemberReference = 9
-    ObjectNull = 10
-    MessageEnd = 11
-    BinaryLibrary = 12
-    ObjectNullMultiple256 = 13
-    ObjectNullMultiple = 14
-    ArraySinglePrimitive = 15
-    ArraySingleObject = 16
-    ArraySingleString = 17
-    MethodCall = 21
-    MethodReturn = 22
+from dnbf4py.format.types import Record, RecordTypeEnum, RecordTypes
 
 
 class DNBinaryFormat:
--- src/dnbf4py/format/types.py
+++ src/dnbf4py/format/types.py
@@ -1,8 +1,29 @@
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any
+from enum import IntEnum
+from typing import Any
 
-if TYPE_CHECKING:
-    from dnbf4py.format.format import RecordTypeEnum
+
+class RecordTypeEnum(IntEnum):
+    SerializedStreamHeader = 0
+    ClassWithId = 1
+    SystemClassWithMembers = 2
+    ClassWithMembers = 3
+    SystemClassWithMembersAndTypes = 4
+    ClassWithMembersAndTypes = 5
+    BinaryObjectString = 6
+    BinaryArray = 7
+    MemberPrimitiveTyped = 8
+    MemberReference = 9
+    ObjectNull = 10
+    MessageEnd = 11
+    BinaryLibrary = 12
+    ObjectNullMultiple256 = 13
+    ObjectNullMultiple = 14
+    ArraySinglePrimitive = 15
+    ArraySingleObject = 16
+    ArraySingleString = 17
+    MethodCall = 21
+    MethodReturn = 22
 
 
 @dataclass
