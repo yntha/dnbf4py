54f86dbcb8793ed214e036e8bc834b20db457e07
yntha <126660548+yntha@users.noreply.github.com>
2024-05-23 13:02:44 -0400

    Add new IntEnum `RecordTypeEnum`

--- src/dnbf4py/format/format.py
+++ src/dnbf4py/format/format.py
@@ -1,6 +1,31 @@
-from typing import *
+from enum import IntEnum
 from pathlib import Path
+from typing import *
+
 from datastream import DeserializingStream, SerializingStream
+
+
+class RecordTypeEnum(IntEnum):
+    SerializedStreamHeader = 0
+    ClassWithId = 1
+    SystemClassWithMembers = 2
+    ClassWithMembers = 3
+    SystemClassWithMembersAndTypes = 4
+    ClassWithMembersAndTypes = 5
+    BinaryObjectString = 6
+    BinaryArray = 7
+    MemberPrimitiveTyped = 8
+    MemberReference = 9
+    ObjectNull = 10
+    MessageEnd = 11
+    BinaryLibrary = 12
+    ObjectNullMultiple256 = 13
+    ObjectNullMultiple = 14
+    ArraySinglePrimitive = 15
+    ArraySingleObject = 16
+    ArraySingleString = 17
+    MethodCall = 21
+    MethodReturn = 22
 
 
 class DNBinaryFormat:
