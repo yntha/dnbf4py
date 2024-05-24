cb6c78c28e0465bcfb022dea60b37b085fdb30db
yntha <126660548+yntha@users.noreply.github.com>
2024-05-23 21:56:59 -0400

    Add new enumeration `MessageFlags`

--- src/dnbf4py/format/types.py
+++ src/dnbf4py/format/types.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from enum import IntEnum
+from enum import IntEnum, IntFlag
 from typing import Any
 
 
@@ -65,6 +65,24 @@ class PrimitiveTypeEnum(IntEnum):
     UInt64 = 16
     Null = 17
     String = 18
+
+
+class MessageFlags(IntFlag):
+    NoArgs = 0x00000001
+    ArgsInline = 0x00000002
+    ArgsIsArray = 0x00000004
+    ArgsInArray = 0x00000008
+    NoContext = 0x00000010
+    ContextInline = 0x00000020
+    ContextInArray = 0x00000040
+    MethodSignatureInArray = 0x00000080
+    PropertiesInArray = 0x00000100
+    NoReturnValue = 0x00000200
+    ReturnValueVoid = 0x00000400
+    ReturnValueInline = 0x00000800
+    ReturnValueInArray = 0x00001000
+    ExceptionInArray = 0x00002000
+    GenericMethod = 0x00008000
 
 
 @dataclass
