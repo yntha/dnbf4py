823e7f5da0ee83679d75ba09d4c6ef606d2200ea
yntha <126660548+yntha@users.noreply.github.com>
2024-05-23 21:20:51 -0400

    Add new enumeration `PrimitiveTypeEnum`

--- src/dnbf4py/format/types.py
+++ src/dnbf4py/format/types.py
@@ -46,6 +46,27 @@ class BinaryArrayTypeEnum(IntEnum):
     RectangularOffset = 5
 
 
+class PrimitiveTypeEnum(IntEnum):
+    Boolean = 1
+    Byte = 2
+    Char = 3
+    Unused = 4
+    Decimal = 5
+    Double = 6
+    Int16 = 7
+    Int32 = 8
+    Int64 = 9
+    SByte = 10
+    Single = 11
+    TimeSpan = 12
+    DateTime = 13
+    UInt16 = 14
+    UInt32 = 15
+    UInt64 = 16
+    Null = 17
+    String = 18
+
+
 @dataclass
 class Record:
     type: RecordTypeEnum
